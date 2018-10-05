c8b2a0bd5c190e993f8e1450ac2aaa142c299d52
vicmatmar <vicmatmar@users.noreply.github.com>
2018-10-05 11:55:31 -0500

    Changed sites to enum Better use of labels using Display(name=) at code level

--- CLWebApplication/Models/FinalTestCount.cs
+++ CLWebApplication/Models/FinalTestCount.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +10,30 @@ namespace CLWebApp.Models
 {
     public class FinalTestReport
     {
-        public SelectListItem Site = new SelectListItem { Value = "8", Text = "Moko" };
+
+        public enum Sites{[Display(Name ="Centralite")] Cl=2, Moko=8}
+        //public List<SelectListItem> Sites { get; } = new List<SelectListItem>
+        //{
+        //    new SelectListItem { Value = "2", Text = "Centralite" },
+        //    new SelectListItem { Value = "8", Text = "Moko" },
+        //};
+
+
+        //public SelectListItem Site = new SelectListItem { Value = "8", Text = "Moko" };
 
         int _siteid = 2;
+        [Required]
+        [Display(Name ="Site:")]
         public int Siteid { get => _siteid; set => _siteid = value; }
 
         int _numberOfDays = 10;
+        [Required]
+        [Display(Name = "Days:")]
         public int NumberOfDays { get => _numberOfDays; set => _numberOfDays = value; }
 
         int _hrOffset = 0;
+        [Required]
+        [Display(Name = "Offset:")]
         public int HrOffset { get => _hrOffset; set => _hrOffset = value; }
 
         public FinalTestCount[] FinalTestCounts { get; set; }
